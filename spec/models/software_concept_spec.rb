027d5c82c40292263cf0d377ef15e34f0398437e
ouin85 <bouinten@yahoo.fr>
2021-06-29 18:38:46 +0200

    add spec on SoftwareConcept model

--- spec/models/software_concept_spec.rb
+++ spec/models/software_concept_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe SoftwareConcept, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:software_concept) { create(:software_concept) }
+
+  it 'should create a valid instance of software_concept' do
+    expect(software_concept).to be_valid
+  end
+
+  describe "associations" do
+    it { should belong_to(:concept) }
+    it { should belong_to(:software) }
+  end
 end
