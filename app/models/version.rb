7894da90173821a5045021cbcba9f627fcfbef38
ouin85 <bouinten@yahoo.fr>
2021-07-09 07:28:24 +0200

    add presence validation of name and corresponding spec

--- app/models/version.rb
+++ app/models/version.rb
@@ -4,6 +4,7 @@ class Version < ApplicationRecord
   has_many :concepts, through: :version_concepts
 
   validates_length_of :name, maximum: 10, minimum: 1
+  validates_presence_of :name
   validates_uniqueness_of :name
   validates_length_of :description, maximum: 1000, minimum: 1
 
--- spec/models/version_spec.rb
+++ spec/models/version_spec.rb
@@ -14,10 +14,10 @@ RSpec.describe Version, type: :model do
   
   context "validations" do
     it { should validate_length_of(:name).is_at_least(1).is_at_most(10) }
-    it { should validate_length_of(:description).is_at_least(1).is_at_most(1000) }
-    
+    it { should validate_presence_of(:name) }
     subject { FactoryBot.build(:version) }
     it { should validate_uniqueness_of(:name) }
+    it { should validate_length_of(:description).is_at_least(1).is_at_most(1000) }
   end
 
   context "public instance methods" do
