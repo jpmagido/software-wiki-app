6e46d9263ca6b7e9061c2eacb15a8850222f770d
ouin85 <bouinten@yahoo.fr>
2021-09-11 18:14:32 +0200

    fix bug with value factory

--- spec/factories/values.rb
+++ spec/factories/values.rb
@@ -2,9 +2,8 @@ require 'ffaker'
 
 FactoryBot.define do
   factory :value do
-    property { nil }
     text { FFaker::Lorem.sentence }
-    description { FFaker::Lorem.paragraph(sentence_count: 7) }
+    description { FFaker::Lorem.sentences.join }
     association :property
   end
 end
